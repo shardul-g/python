6c9eda9ab82c186b8dc3eb8c076185db69de69c2
shardul-g <godageshardul95@gmail.com>
2025-03-27 12:36:20 +0530

    Day 1
    Day 1

---
+++ Day1/brand name generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the brand generator")
+City = input("Which city you grew up in ?\n")
+pet_name = input("What is you pet name ?\n")
+print("Your brand name is: " + City + "_" + pet_name)
+
---
+++ Day1/print.py
@@ -0,0 +1,13 @@
+# print("Hello world!")
+
+
+# print("Hello world!\nHello world!")  # adding a new line in print statement
+#
+# print("Hello" + " " + "Shardul")  # concatenation
+
+# print("Hello " + input("What is your name?") + "!")  # taking input from the user and printing it.
+
+name = input("What is your name ?")
+
+# print(name)
+print(len(name))
